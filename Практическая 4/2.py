1304f5d95fc649c0873a86bc00975ca32f765348
Yannii13 <yanazemskova13@bk.ru>
2025-11-12 11:32:45 +0300

    Update 2.py

--- Практическая 4/2.py
+++ Практическая 4/2.py
@@ -1,7 +1,7 @@
 import math
-x1=float(input('введите координату x1 '))
-x2=float(input('введите координату x2 '))
-y1=float(input('введите координату y1 '))
-y2=float(input('введите координату y2 '))
-evk1_cast=math.sqrt((x1-x2)**2+ (y1-y2)**2)
-print(evk1_cast)
+x1=float(input('введите x1: '))
+x2=float(input('введите x2: '))
+y1=float(input('введите y1: '))
+y2=float(input('введите y2: '))
+evkl_num=math.sqrt((x1-x2)**2+ (y1-y2)**2)
+print(evkl_num)
